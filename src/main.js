139efcd991d5706daeaf67d8a718e6850359e4d8
Hiroki Yokouchi <8845musign@gmail.com>
2023-05-13 02:04:18 +0900

    client_msg_idごとに処理をロックし、再送を防止

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 const OPEN_AI_KEY = PropertiesService.getScriptProperties().getProperty('OPEN_AI_KEY');
 const SLACK_BOT_TOKEN = PropertiesService.getScriptProperties().getProperty('SLACK_BOT_TOKEN');
+const cache = CacheService.getScriptCache();
 
 if (OPEN_AI_KEY === null) {
   throw new Error('OPEN_AI_KEY is not set');
@@ -10,6 +11,21 @@ if (SLACK_BOT_TOKEN === null) {
 }
 
 const app = SlackApp.create(SLACK_BOT_TOKEN);
+
+function lockProcessByClientMsgId(client_msg_id) {
+  // client_msg_idをキャッシュに追加
+  cache.put(`process_${client_msg_id}`, client_msg_id, 60 * 5);
+  Logger.log(`lock: ${client_msg_id}`);
+}
+
+function isProcessing(client_msg_id) {
+  return cache.get(`process_${client_msg_id}`) !== null;
+}
+
+function unLockProcessByClientMsgId(client_msg_id) {
+  cache.remove(`process_${client_msg_id}`);
+  Logger.log(`unlock: ${client_msg_id}`);
+}
 
 function doPost(e) {
   Logger.log('-------- run --------');
@@ -28,14 +44,23 @@ function doPost(e) {
       return ContentService.createTextOutput(json.challenge);
     }
   }
-  catch (ex) {
-    Logger.log(ex);
+  catch (e) {
+    Logger.log(e);
   }
+
+  const { text, client_msg_id } = postData.event;
+
+  if (isProcessing(client_msg_id)) {
+    return;
+  }
+
+  lockProcessByClientMsgId(client_msg_id);
 
   try {
     const mentinoReg = /<@.*?>/;
-    app.chatPostMessage(postData.event.channel, `${postData.event.text.replace(mentinoReg, '')}と言われた`);
+    app.chatPostMessage(postData.event.channel, `${text.replace(mentinoReg, '')}と言われた`);
   } catch (e) {
     Logger.log(e);
+    unLockProcessByClientMsgId(client_msg_id);
   }
 }
